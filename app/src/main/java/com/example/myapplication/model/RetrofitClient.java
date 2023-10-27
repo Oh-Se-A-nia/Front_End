bc17cbcdc063cfb82cdbcfaba316363de8b4e226
ohsung0722 <99doldol@gmail.com>
2023-10-27 23:05:54 +0900

    [ADD] 레트로핏 인스턴스 싱글톤으로 생성

--- app/src/main/java/com/example/myapplication/model/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/model/RetrofitClient.java
@@ -1,4 +1,25 @@
 package com.example.myapplication.model;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitClient {
+
+    private final static String HOST_URL = "base_url";
+    private static RetrofitClient instance = null;
+
+    private RetrofitClient(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(HOST_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static RetrofitClient getInstance(){
+        if(instance == null){
+            instance = new RetrofitClient();
+        }
+        return instance;
+    }
+
 }
