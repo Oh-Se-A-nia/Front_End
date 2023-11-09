59a9930aafc300aa89f8d0385a9fed38cb8a115e
ohsung0722 <99doldol@gmail.com>
2023-11-09 16:40:10 +0900

    [ADD] 사용자 위치 권한설정 및 확인 기능 추가

--- app/src/main/java/com/example/myapplication/view/activity/EcoTagActivity.java
+++ app/src/main/java/com/example/myapplication/view/activity/EcoTagActivity.java
@@ -2,11 +2,13 @@ package com.example.myapplication.view.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,12 +16,28 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.databinding.ActivityCircleMenuBinding;
 import com.example.myapplication.databinding.ActivityEcoTagBinding;
 import com.example.myapplication.view.Constants;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Granularity;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -28,6 +46,15 @@ import java.io.IOException;
 public class EcoTagActivity extends AppCompatActivity {
 
     private ActivityEcoTagBinding binding;
+    public static final int DEFAULT_LOCATION_REQUEST_PRIORITY = Priority.PRIORITY_BALANCED_POWER_ACCURACY;
+    public static final long DEFAULT_LOCATION_REQUEST_INTERVAL = 20000L;
+    private static final int GPS_UTIL_LOCATION_RESOLUTION_REQUEST_CODE = 101;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationRequest locationRequest;
+
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +70,7 @@ public class EcoTagActivity extends AppCompatActivity {
         binding.imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                registerImageWithAlbum();
+                registerImageWithCamera();
             }
         });
     }
@@ -105,4 +132,66 @@ public class EcoTagActivity extends AppCompatActivity {
                 }
             }
     );
+
+    private void checkLocationSetting(){
+
+        locationRequest = new LocationRequest.Builder(DEFAULT_LOCATION_REQUEST_INTERVAL)
+                .setGranularity(Granularity.GRANULARITY_FINE)
+                .setPriority(DEFAULT_LOCATION_REQUEST_PRIORITY)
+                .build();
+
+        SettingsClient settingsClient = LocationServices.getSettingsClient(EcoTagActivity.this);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).setAlwaysShow(true);
+        settingsClient.checkLocationSettings(builder.build())
+                .addOnSuccessListener(EcoTagActivity.this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(EcoTagActivity.this);
+                        //noinspection MissingPermission
+                        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
+                    }
+                })
+                .addOnFailureListener(EcoTagActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch(statusCode){
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(EcoTagActivity.this, GPS_UTIL_LOCATION_RESOLUTION_REQUEST_CODE);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.w("TAG", "unable to start resolution for result due to " + sie.getLocalizedMessage());
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "location settings are inadequate, and cannot be fixed here. Fix in Settings.";
+                                Log.e("TAG", errorMessage);
+                        }
+                    }
+                });
+    }
+
+    private LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult){
+            super.onLocationResult(locationResult);
+            longitude = locationResult.getLastLocation().getLongitude();
+            latitude = locationResult.getLastLocation().getLatitude();
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        }
+    };
+
+    public double setCurrentLatitude(){
+        System.out.println(latitude);
+        return latitude;
+    }
+
+    public double setCurrentLongitude(){
+        System.out.println(longitude);
+        return longitude;
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/view/activity/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/view/activity/MenuActivity.java
@@ -1,12 +1,21 @@
 package com.example.myapplication.view.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -17,11 +26,28 @@ import com.example.myapplication.view.fragment.ChartFragment;
 import com.example.myapplication.view.fragment.CommunityFragment;
 import com.example.myapplication.view.fragment.MapFragment;
 import com.example.myapplication.view.fragment.ProfileFragment;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Granularity;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MenuActivity extends AppCompatActivity {
 
     private ActivityMenuBinding binding;
+    private static final int GPS_UTIL_LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private static final int GPS_UTIL_LOCATION_RESOLUTION_REQUEST_CODE = 101;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -50,6 +76,7 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+        checkLocationPermission();
         clickCameraButton();
     }
 
@@ -68,6 +95,67 @@ public class MenuActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
+    }
+
+    private void checkLocationPermission(){
+        int accessLocation = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if(accessLocation == PackageManager.PERMISSION_GRANTED){
+            //checkLocationSetting();
+        } else{
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}
+                    , GPS_UTIL_LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GPS_UTIL_LOCATION_RESOLUTION_REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+               // checkLocationSetting();
+            }
+            else{
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == GPS_UTIL_LOCATION_PERMISSION_REQUEST_CODE){
+            for(int i = 0; i < permissions.length; i++){
+                if(Manifest.permission.ACCESS_FINE_LOCATION.equals(permissions[i])){
+                    if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                        //checkLocationSetting();
+                    }
+                    else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MenuActivity.this);
+                        builder.setTitle("위치 권한이 꺼져있습니다");
+                        builder.setMessage("위치 정보 권한이 필요합니다.\\n\\n[설정]->[권한]에서 '위치' 항목을 사용으로 설정해 주세요.");
+                        builder.setPositiveButton("설정으로 가기", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent intent = new Intent();
+                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                intent.setData(uri);
+                                startActivity(intent);
+                            }
+                        }).setNegativeButton("종료", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+                    break;
+                }
+            }
+        }
     }
 
 }
